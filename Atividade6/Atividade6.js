86a182cd63c3841f69dd1a4011f8b847bd03e3f0
Edimilson Borges <edimilson-borges159@hotmail.com>
2025-05-02 20:15:43 -0300

    feat: adição de validação

--- Atividade6/Atividade6.js
+++ Atividade6/Atividade6.js
@@ -8,22 +8,32 @@
 
 prompt = require('prompt-sync')();
 
-const ladoA = Number(prompt("Digite o valor do lado A:"));
-const ladoB = Number(prompt("Digite o valor do lado B:"));
-const ladoC = Number(prompt("Digite o valor do lado C:"));
+const ladoA = Number(prompt("Digite o valor do lado A: "));
+const ladoB = Number(prompt("Digite o valor do lado B: "));
+const ladoC = Number(prompt("Digite o valor do lado C: "));
 
 function eTriangulo() {
     return (ladoA < (ladoB + ladoC)) && (ladoB < (ladoA + ladoC)) ? true : false;
 }
 
-if (eTriangulo()) {
-    if ((ladoA !== ladoB) && (ladoB !== ladoC)) {
-        console.log("Triângulo escaleno");
-    } else if ((ladoA === ladoB) && (ladoB === ladoC)) {
-        console.log("Triângulo eqüilátero");
-    } else if ((ladoA === ladoB) || (ladoA === ladoC) || (ladoB === ladoC)) {
-        console.log("Triângulo isósceles");
-    }
-} else {
+function eNumerosValidos() {
+    return (!isNaN(ladoA) && ladoA > 0) && (!isNaN(ladoB) && ladoB > 0) && (!isNaN(ladoC) && ladoC > 0);
+}
+
+if (!eNumerosValidos()) {
+    console.log("Os valores informados são inválidos");
+    return;
+}
+
+if (!eTriangulo()) {
     console.log("Os valores digitados não formam um triângulo");
+    return;
+}
+
+if ((ladoA !== ladoB) && (ladoB !== ladoC)) {
+    console.log("Triângulo escaleno");
+} else if ((ladoA === ladoB) && (ladoB === ladoC)) {
+    console.log("Triângulo eqüilátero");
+} else if ((ladoA === ladoB) || (ladoA === ladoC) || (ladoB === ladoC)) {
+    console.log("Triângulo isósceles");
 }
